e6f2497111f68a1a4c0cf84b6f255203a2f6249a
neema <neemadas@gmail.com>
2023-02-28 16:05:24 +0000

    fix: Add typescript to all

--- client/src/menu/options/browse_posts/browse_posts.ts
+++ client/src/menu/options/browse_posts/browse_posts.ts
@@ -1,23 +1,57 @@
 import { fetchPost } from "../../../api/fetch_post";
-import { clear, print, prompt, printNewLine } from "../../../ui/console";
+import { print, prompt, printNewLine, clear } from "../../../ui/console";
+import {
+	Post,
+	postFoundType,
+	postNotFoundType,
+} from "../../../types/posts.types";
+import { states } from "../../../states/states";
+import { validateId } from "../../../ErrorHandling/validation";
 
-export async function browsePosts() {
-	clear("nope");
+export async function browsePosts(): Promise<void | string> {
+	clear();
 
-	const desiredPostId = await prompt("Enter Post ID");
+	const desiredPostId: string = await prompt("Enter Post ID");
 
-	// TODO: should we validate this?!
+	// Checking if id is a number
+	if (validateId(desiredPostId)) {
+		print(`📨 Fetching post "${desiredPostId}...`);
 
-	print(`📨 Fetching post "${desiredPostId}...`);
+		const result = await fetchPost(desiredPostId);
 
-	const result = await fetchPost(desiredPostId);
+		print(`🥳 Received post:`);
 
-	print(`🥳 Received post:`);
+		// Checking if result is not an error message
+		if (typeof result !== "string") {
+			// Checking if result has postFound === true or false
+			if (result.postFound) {
+				const data: Post = {
+					id: result.id,
+					title: result.title,
+					text: result.text,
+					author: result.author,
+				};
 
-	console.log(result);
-
-	printNewLine();
-	await prompt("⌨️ Press [ENTER] to return to the main menu! 🕶️");
-
-	return result;
+				console.log(data);
+				printNewLine();
+				await prompt("⌨️ Press [ENTER] to return to the main menu! 🕶️");
+			} else {
+				clear();
+				console.log(`There are no posts with id: ${desiredPostId}`);
+				printNewLine();
+				await prompt("⌨️ Press [ENTER] to return to the main menu! 🕶️");
+			}
+		} else {
+			clear();
+			console.log(result);
+			printNewLine();
+			await prompt("⌨️ Press [ENTER] to return to the main menu! 🕶️");
+		}
+	} else {
+		clear();
+		console.log("Enter a valid number for the id");
+		printNewLine();
+		await prompt("⌨️ Press [ENTER] to return to the main menu! 🕶️");
+		return states.UNKNOWN;
+	}
 }
