eebbe384507da76decf1af24a43affbe1b06dc28
neema <neemadas@gmail.com>
2023-03-04 02:59:30 +0000

    fix: Change the path to delete user by id

--- client/src/api/get_user_to_delete.ts
+++ client/src/api/get_user_to_delete.ts
@@ -3,13 +3,15 @@ import { baseUrl } from "./base_url";
 import { errorMessage } from "../ErrorHandling/errorMessage";
 import { UserDeletedResponseType } from "../types/response.types";
 
-export async function getUserToDelete(
-	username: string
-): Promise<UserDeletedResponseType | string> {
+export async function getUserToDelete(id: string): Promise<boolean | string> {
 	try {
-		const response = await fetch(baseUrl + "/api/users/delete/" + username);
-		const result = await response.json();
-		return result;
+		const response = await fetch(baseUrl + "/api/users/" + id, {
+			method: "DELETE",
+		});
+
+		const { deleted } = (await response.json()) as UserDeletedResponseType;
+
+		return deleted;
 	} catch (err) {
 		return errorMessage(err);
 	}
